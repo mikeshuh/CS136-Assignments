69645faa8792714d7c4d0fece4447e99a002da7e
mikeshuh <michael.stephen.huh@gmail.com>
2024-09-14 22:04:32 -0700

    9-14

--- coding/project1/netpbm/main.c
+++ coding/project1/netpbm/main.c
@@ -12,8 +12,16 @@
 #include "netpbm.h"
 #include <stdio.h>
 
+// added
+#include <time.h>
 
-
+// Function prototypes
+Image toBlackWhite(Image img, int intensityThreshold);
+Image expand(Image img);
+Image shrink(Image img);
+Image expand4(Image img);
+Image shrink4(Image img);
+Image noiseImage(Image img, float p);
 
 int main(int argc, const char * argv[]) {
     //-------------------------------------------------------------------------------
@@ -24,22 +32,22 @@ int main(int argc, const char * argv[]) {
     //    Image inputImage = readImage("/Users/michaelhuh/Documents/SJSU/24-25/FA_24/CS-136_comp-vision/CS136-Assignments/coding/project1/netpbm/car.ppm");
        
        // PC
-       Image inputImage = readImage("/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/car.ppm");
+      //  Image inputImage = readImage("/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/car.ppm");
 
        //-------------------------------------------------------------------------------
        // COMMENT THIS FUNCTION IF YOU DON'T WANT IT TO RUN EVEY TIME
-    Image rotatedImage = createImage(inputImage.height, inputImage.width);
-    Image invertedImage = createImage(inputImage.height, inputImage.width);
-       for (int x = 0; x < inputImage.width; x++)
-           for (int y = 0; y < inputImage.height; y++)
-           {
-               rotatedImage.map[y][x] = inputImage.map[inputImage.height - y - 1][inputImage.width - x - 1];
+   //  Image rotatedImage = createImage(inputImage.height, inputImage.width);
+   //  Image invertedImage = createImage(inputImage.height, inputImage.width);
+   //     for (int x = 0; x < inputImage.width; x++)
+   //         for (int y = 0; y < inputImage.height; y++)
+   //         {
+   //             rotatedImage.map[y][x] = inputImage.map[inputImage.height - y - 1][inputImage.width - x - 1];
 
-               invertedImage.map[y][x].r = 255 - inputImage.map[y][x].r;
-               invertedImage.map[y][x].g = 255 - inputImage.map[y][x].g;
-               invertedImage.map[y][x].b = 255 - inputImage.map[y][x].b;
-               // Let's just ignore 'i' here; it's irrelevant if we want to save image as PPM.
-           }
+   //             invertedImage.map[y][x].r = 255 - inputImage.map[y][x].r;
+   //             invertedImage.map[y][x].g = 255 - inputImage.map[y][x].g;
+   //             invertedImage.map[y][x].b = 255 - inputImage.map[y][x].b;
+   //             // Let's just ignore 'i' here; it's irrelevant if we want to save image as PPM.
+   //         }
 
        // Mac
     //    writeImage(rotatedImage, "/Users/michaelhuh/Documents/SJSU/24-25/FA_24/CS-136_comp-vision/CS136-Assignments/coding/project1/netpbm/rotated.pbm");
@@ -48,45 +56,81 @@ int main(int argc, const char * argv[]) {
     //    writeImage(inputImage, "/Users/michaelhuh/Documents/SJSU/24-25/FA_24/CS-136_comp-vision/CS136-Assignments/coding/project1/netpbm/black-white.pbm");
 
        // PC
-       writeImage(rotatedImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/rotated.pbm");
-       writeImage(invertedImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/inverted.pbm");
-       writeImage(inputImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/gray.pgm");
-       writeImage(inputImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/black-white.pbm");
+      //  writeImage(rotatedImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/rotated.pbm");
+      //  writeImage(invertedImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/inverted.pbm");
+      //  writeImage(inputImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/gray.pgm");
+      //  writeImage(inputImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/black-white.pbm");
 
     //-------------------------------------------------------------------------------
-       //create blackWhiteImage:
 
+      // inputImage (read from file)
+      Image inputImage = readImage("/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/text_image.ppm");
 
-       // create noiseImage:
+      // blackWhiteImage
+      Image blackWhiteTextImage = toBlackWhite(inputImage, 128);
+      writeImage(blackWhiteTextImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/black-white.pbm");
 
+      // Denoise
+      // S = shrink; E = Expand
+      // SEES & ESSE
+         // S & E 8-connectedness
+         Image cleanedTextImageSEES = shrink(expand(expand(shrink(blackWhiteTextImage))));
+         Image cleanedTextImageESSE = expand(shrink(shrink(expand(blackWhiteTextImage))));
+         
+         // S 4-connectedness; E 8-connectedness
+         Image cleanedTextImageSEES_s4 = shrink4(expand(expand(shrink4(blackWhiteTextImage))));
+         Image cleanedTextImageESSE_s4 = expand(shrink4(shrink4(expand(blackWhiteTextImage))));
 
-       // create expandImage:
+         // S 8-connectedness; E 4-connectedness
+         Image cleanedTextImageSEES_e4 = shrink(expand4(expand4(shrink(blackWhiteTextImage))));
+         Image cleanedTextImageESSE_e4 = expand4(shrink(shrink(expand4(blackWhiteTextImage))));
 
+         // S & E 4-connectedness
+         Image cleanedTextImageSEES_b4 = shrink4(expand4(expand4(shrink4(blackWhiteTextImage))));
+         Image cleanedTextImageESSE_b4 = expand4(shrink4(shrink4(expand4(blackWhiteTextImage))));
 
-       // create shrinkImage:
+      // Write to files
+      writeImage(cleanedTextImageSEES, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_SEES.pbm");
+      writeImage(cleanedTextImageESSE, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_ESSE.pbm");
+      writeImage(cleanedTextImageSEES_s4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_SEES_s4.pbm");
+      writeImage(cleanedTextImageESSE_s4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_ESSE_s4.pbm");
+      writeImage(cleanedTextImageSEES_e4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_SEES_e4.pbm");
+      writeImage(cleanedTextImageESSE_e4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_ESSE_e4.pbm");
+      writeImage(cleanedTextImageSEES_b4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_SEES_b4.pbm");
+      writeImage(cleanedTextImageESSE_b4, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/image_cleaned_ESSE_b4.pbm");
 
+      // best res: sees_b4
+
+      // Create noisedImage and write to files
+      Image noisedImage = noiseImage(cleanedTextImageSEES_b4, 5.0f);
+      writeImage(noisedImage, "/Users/mshuh/SJSU/FA24/CS136-CompVision/Assignments/coding/project1/netpbm/noise.pbm");
 
        //-------------------------------------------------------------------------------
        // Uncomment this after you finish your homework
-       // Function that does threshold, noise and numbers of spanding and shrinking
+       // Function that does threshold, noise and numbers of expanding and shrinking
        // COMMENT THIS FUNCTION IF YOU DON'T WANT IT TO RUN EVEY TIME
        //function_readImage();
 
         /* Delete back and white, noise, Expand and Shrink */
-       deleteImage(inputImage);
-       deleteImage(rotatedImage);
-       deleteImage(invertedImage);
+      //  deleteImage(inputImage);
+      //  deleteImage(rotatedImage);
+      //  deleteImage(invertedImage);
+      deleteImage(inputImage);
+      deleteImage(blackWhiteTextImage);
+      deleteImage(cleanedTextImageSEES);
+      deleteImage(cleanedTextImageESSE);
+      deleteImage(cleanedTextImageSEES_s4);
+      deleteImage(cleanedTextImageESSE_s4);
+      deleteImage(cleanedTextImageSEES_e4);
+      deleteImage(cleanedTextImageESSE_e4);
+      deleteImage(cleanedTextImageSEES_b4);
+      deleteImage(cleanedTextImageESSE_b4);
+      deleteImage(noisedImage);
 
+      printf("Program ends ... ");
 
-
-
-       printf("Program ends ... ");
-
-
-       return 0;
+      return 0;
 }
-
-
 
 //-------------------------------function_imageBlackWhite-------------------------------------------------
 /* function that receives an Image structure and an intensity threshold
@@ -94,18 +138,111 @@ int main(int argc, const char * argv[]) {
  or white (intensity = 255). The function should return an Image structure
  containing the result. */
 
+Image toBlackWhite(Image img, int intensityThreshold) {
+   // create new img
+   Image bwImg = createImage(img.height, img.width);
 
-
+   // iterate over original img
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         // if img <= intensity threshold --> obj (i = 0)
+         if (img.map[i][j].i <= intensityThreshold) {
+            bwImg.map[i][j].i = 0;
+         }
+         // otherwise --> background (i = 255)
+         else {
+            bwImg.map[i][j].i = 255;
+         }
+         // set r, g, b to match i
+         bwImg.map[i][j].r = bwImg.map[i][j].g = bwImg.map[i][j].b = bwImg.map[i][j].i;
+      }
+   }
+   return bwImg;
+}
 
 //--------------------------------Expand function-----------------------------------------------------------
 /* Expand operation */
 
+// 8-connectedness expand
+Image expand(Image img) {
+   // create new img
+   Image expImg = createImage(img.height, img.width);
+
+   // copy original img to new img
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         expImg.map[i][j] = img.map[i][j];
+      }
+   }
+
+   // iterate over original img
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         // check if pixel is black/obj
+         if (img.map[i][j].i == 0) {
+            // make neighboring pixels black mapped to new img
+            for (int x = -1; x < 2; x++) {
+               for (int y = -1; y < 2; y++) {
+                  //adjusted idx
+                  int s = i + x;
+                  int t = j + y;
+                  // check if neighbor is in bounds
+                  if(s >= 0 && s < img.height && t >= 0 && t < img.width) {
+                     // set i to 0
+                     expImg.map[s][t].i = 0;
+                     // set r, g, b to match i
+                     expImg.map[s][t].r = expImg.map[s][t].g = expImg.map[s][t].b = expImg.map[s][t].i;
+                  }
+               }
+            }
+         }
+      }
+   }
+   return expImg;
+}
 
 
 
 //--------------------------------Shrink function-----------------------------------------------------------
 /* Shrink operation */
 
+// 8-connectedness shrink
+Image shrink(Image img) {
+   // create new img
+   Image shrinkImg = createImage(img.height, img.width);
+
+   // copy original img to new img
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         shrinkImg.map[i][j] = img.map[i][j];
+      }
+   }
+
+   // iterate over original img
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         // check if pixel is white/background
+         if (img.map[i][j].i == 255) {
+            // make neighboring pixels white mapped to new img
+            for (int x = -1; x < 2; x++) {
+               for (int y = -1; y < 2; y++) {
+                  //adjusted idx
+                  int s = i + x;
+                  int t = j + y;
+                  // check if neighbor is in bounds
+                  if(s >= 0 && s < img.height && t >= 0 && t < img.width) {
+                     // set i to 255
+                     shrinkImg.map[s][t].i = 255;
+                     // set r, g, b to match i
+                     shrinkImg.map[s][t].r = shrinkImg.map[s][t].g = shrinkImg.map[s][t].b = shrinkImg.map[s][t].i;
+                  }
+               }
+            }
+         }
+      }
+   }
+   return shrinkImg;
+}
 
 
 //--------------------------------noise function-----------------------------------------------------------
@@ -114,3 +251,107 @@ int main(int argc, const char * argv[]) {
  (in percent) that each pixel in the image will be flipped, i.e.,
  turned from black to white or vice versa.
  */
+Image noiseImage(Image img, float p) {
+    Image noiseImg = createImage(img.height, img.width);
+    
+    // Seed the random number generator
+    srand((unsigned int)time(NULL));
+    
+    for (int i = 0; i < img.height; i++) {
+        for (int j = 0; j < img.width; j++) {
+            int intensity = img.map[i][j].i;
+            
+            // Generate a random float between 0 and 1
+            float random = (float)rand() / RAND_MAX;
+            
+            if (random <= p / 100.0f) {  // Convert p to a probability between 0 and 1
+                noiseImg.map[i][j].i = (intensity == 0) ? 255 : 0;
+            } else {
+                noiseImg.map[i][j].i = intensity;
+            }
+            
+            // Set r, g, b to match i
+            noiseImg.map[i][j].r = noiseImg.map[i][j].g = noiseImg.map[i][j].b = noiseImg.map[i][j].i;
+        }
+    }
+    
+    return noiseImg;
+}
+
+
+
+// Expand operation using 4-connectedness
+Image expand4(Image img) {
+   // Create a new image to store the result
+   Image expImg = createImage(img.height, img.width);
+
+   // Copy the original image to the new image
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         expImg.map[i][j] = img.map[i][j];
+      }
+   }
+
+   // Define the neighbor offsets for 4-connectedness
+   int dx[] = {-1, 0, 1, 0};
+   int dy[] = {0, 1, 0, -1};
+
+   // Iterate over each pixel in the original image
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         // Check if the current pixel is black (object)
+         if (img.map[i][j].i == 0) {
+            // Set the 4-connected neighbors to black in the new image
+            for (int k = 0; k < 4; k++) {
+               int s = i + dx[k];
+               int t = j + dy[k];
+               // Check if neighbor is within bounds
+               if (s >= 0 && s < img.height && t >= 0 && t < img.width) {
+                  expImg.map[s][t].i = 0;
+                  // Set r, g, b to match the intensity
+                  expImg.map[s][t].r = expImg.map[s][t].g = expImg.map[s][t].b = 0;
+               }
+            }
+         }
+      }
+   }
+   return expImg;
+}
+
+// Shrink operation using 4-connectedness
+Image shrink4(Image img) {
+   // Create a new image to store the result
+   Image shrinkImg = createImage(img.height, img.width);
+
+   // Initialize the new image to the original image
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         shrinkImg.map[i][j] = img.map[i][j];
+      }
+   }
+
+   // Define the neighbor offsets for 4-connectedness
+   int dx[] = {-1, 0, 1, 0};
+   int dy[] = {0, 1, 0, -1};
+
+   // Iterate over each pixel in the original image
+   for (int i = 0; i < img.height; i++) {
+      for (int j = 0; j < img.width; j++) {
+         // Check if the current pixel is white (background)
+         if (img.map[i][j].i == 255) {
+            // Set the 4-connected neighbors to white in the new image
+            for (int k = 0; k < 4; k++) {
+               int s = i + dx[k];
+               int t = j + dy[k];
+               // Check if neighbor is within bounds
+               if (s >= 0 && s < img.height && t >= 0 && t < img.width) {
+                  shrinkImg.map[s][t].i = 255;
+                  // Set r, g, b to match the intensity
+                  shrinkImg.map[s][t].r = shrinkImg.map[s][t].g = shrinkImg.map[s][t].b = 255;
+               }
+            }
+         }
+      }
+   }
+   return shrinkImg;
+}
