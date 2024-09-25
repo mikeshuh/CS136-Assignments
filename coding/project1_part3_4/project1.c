3e6349411be3b5f9a564b5f55924fef6d4fe529b
mikeshuh <michael.stephen.huh@gmail.com>
2024-09-25 04:08:23 -0700

    project1_part3_4

--- coding/project1_part3_4/project1.c
+++ coding/project1_part3_4/project1.c
@@ -5,7 +5,7 @@
 // project1_part3_4
 // 
 // created by Michael Huh 9/23/24
-// last modified 9/23/24
+// last modified 9/25/24
 // 
 // referenced https://www.geeksforgeeks.org/generating-random-number-range-c/
 // for generating random numbers in c
@@ -24,7 +24,8 @@
 #include "netpbm.h"
 #include <time.h>
 
-#define MAX_EQUIVALENCES 1000000
+// change based on image size
+#define MAX_EQUIVALENCES 100000
 
 // function prototypes
 Image function_imageBlackWhite(Image img, int intensityThreshold);
@@ -32,9 +33,12 @@ Image expand(Image img);
 Image shrink(Image img);
 Image function_noiseImage(Image img, float p);
 Matrix labelComponents(Image img);
-Image colorAndCountComponents(Image img, Matrix labelMatrix, int *letterCount, int sizeThreshold);
+Image colorAndCountComponents(Image img, Matrix labelMatrix, int sizeThreshold);
 
 int main(int argc, const char * argv[]) {
+    // seed rand function
+    srand(time(0));
+
     // inputImage (read image file)
     Image inputImage = readImage("text_image.ppm");
 
@@ -47,17 +51,11 @@ int main(int argc, const char * argv[]) {
     // noise; probability = 5%
     Image noisedImage = function_noiseImage(cleanedImage, 5.0f);
 
-    // label image components
+    // label components
     Matrix labeledImageMatrix = labelComponents(cleanedImage);
 
-    Image matrixToImage = matrix2Image(labeledImageMatrix, 0, 1);
-    writeImage(matrixToImage, "help.pgm");
-    deleteImage(matrixToImage);
-
-    // color and count components
-    int count = 0;
-    Image coloredImage = colorAndCountComponents(cleanedImage, labeledImageMatrix, &count, 100);
-    printf("Number of Letters: %d\n", count);
+    // color and count components; size threshold = 150
+    Image coloredImage = colorAndCountComponents(cleanedImage, labeledImageMatrix, 150);
 
     // write imgs to files
     writeImage(blackWhiteImage, "black-white.pbm");
@@ -65,11 +63,12 @@ int main(int argc, const char * argv[]) {
     writeImage(noisedImage, "noise.pbm");
     writeImage(coloredImage, "text_colored.ppm");
 
-    // free memory; delete imgs
+    // free memory; delete imgs and label matrix
     deleteImage(inputImage);
     deleteImage(blackWhiteImage);
     deleteImage(cleanedImage);
     deleteImage(noisedImage);
+    deleteMatrix(labeledImageMatrix);
     deleteImage(coloredImage);
 
     printf("Program ends ...");
@@ -199,9 +198,6 @@ Image function_noiseImage(Image img, float p) {
     //create new img
     Image noiseImg = createImage(img.height, img.width);
     
-    // set seed for rand() function
-    srand(time(0));
-    
     // iterate over original img
     for (int i = 0; i < img.height; i++) {
         for (int j = 0; j < img.width; j++) {
@@ -224,15 +220,15 @@ Image function_noiseImage(Image img, float p) {
     return noiseImg;
 }
 
-//---------------------------4-neighbor region/component labeling-------------------------------------------
+//-----------------------------component labeling------------------------------------------------------
 
-// stuct to hold equivalence pairs
+// stuct to for equivalence pairs
 typedef struct {
     int label1;
     int label2;
 } EquivalencePair;
 
-// struct to hold equivalence table
+// struct to for equivalence table
 typedef struct {
     EquivalencePair *pairs;
     int count;
@@ -257,69 +253,84 @@ void addEquivalence(EquivalenceTable *et, int label1, int label2) {
     }
 }
 
-// 4-connectedness component labeling
+// label components of image
 Matrix labelComponents(Image img) {
-    Matrix labelMatrix = createMatrix(img.height, img.width);   // initialize label matrix
+    Matrix labelMatrix = createMatrix(img.height, img.width);   // initialize label matrix; 0 = unlabeled
     EquivalenceTable et = createEquivalenceTable();             // initialize equivalence table
     int currentLabel = 1;                                       // initialize lable counter
 
-    // first pass 
+    // first pass; add labels and equivalences
     for (int i = 0; i < img.height; i++) {
         for (int j = 0; j < img.width; j++) {
-            // if is obj
+            // if pixel is obj
             if (img.map[i][j].i == 0) {
                 int up = (i > 0) ? (int)labelMatrix.map[i-1][j] : 0;        // get label of above pixel
                 int left = (j > 0) ? (int)labelMatrix.map[i][j-1] : 0;      // get label of left pixel
                 
-                if (up == 0 && left == 0) {                     // if above and left both background
-                    labelMatrix.map[i][j] = currentLabel++;     // add new label and increment
-                }
-                else if (up != 0 && left == 0) {                // if above labeled; left background
-                    labelMatrix.map[i][j] = up;                 // set curr pixel label to same as above pixel
-                }
-                else if (left != 0 && up == 0) {                // if left labeled; above background
-                    labelMatrix.map[i][j] = left;               // set curr pixel label to same as left pixel
-                }
-                else{                                           // if above and left both labeled
-                    labelMatrix.map[i][j] = up;                 // set curr pixel label to same as above pixel ()
+                if (up == 0 && left == 0) {                     // if above and left both unlabeled (0)
+                    labelMatrix.map[i][j] = currentLabel++;         // add new label and increment
+                } else if (up != 0 && left == 0) {              // if above labeled; left unlabeled
+                    labelMatrix.map[i][j] = up;                     // set curr pixel label to above pixel label
+                } else if (left != 0 && up == 0) {              // if left labeled; above unlabeled
+                    labelMatrix.map[i][j] = left;                   // set curr pixel label to left pixel label
+                } else{                                         // if above and left both labeled
+                    labelMatrix.map[i][j] = up;                     // set curr pixel label to above pixel label
                     if (up != left) {                               // if above and left labels different
-                        addEquivalence(&et, up, left);              // add to equivalence table
+                        addEquivalence(&et, up, left);                  // add to equivalence table
                     }
                 }
             }
         }
     }
 
-    // process equivalences; find root labels
-        // create label map and load current labels
-        int *labelMap = (int *)malloc(currentLabel * sizeof(int));
-        for (int i = 0; i < currentLabel; i++) {
-            labelMap[i] = i;
+    // allocate memory and create label map
+    int *labelMap = (int *)malloc(currentLabel * sizeof(int));
+    // initialize each label to map to itself
+    for (int i = 0; i < currentLabel; i++) {
+        labelMap[i] = i;
+    }
+
+    // process equivalences found in first pass
+    for (int i = 0; i < et.count; i++) {
+        int label1 = et.pairs[i].label1;
+        int label2 = et.pairs[i].label2;
+        int root1 = label1;
+        int root2 = label2;
+        
+        // follow mapping chain to find root of label1 and label2
+        while (labelMap[root1] != root1) {
+            root1 = labelMap[root1];
         }
-
-        // path compression
-        for (int i = 0; i < et.count; i++) {
-            int label1 = et.pairs[i].label1;
-            int label2 = et.pairs[i].label2;
-            int root1 = label1;
-            int root2 = label2;
-
-            while (labelMap[root1] != root1) root1 = labelMap[root1];
-            while (labelMap[root2] != root2) root2 = labelMap[root2];
-
+        while (labelMap[root2] != root2) {
+            root2 = labelMap[root2];
+        }
+        
+        // if roots are different, merge them
+        if (root1 != root2) {
+            // map larger root to smaller root
             if (root1 < root2) {
                 labelMap[root2] = root1;
-            } else if (root2 < root1) {
+            } else {
                 labelMap[root1] = root2;
             }
         }
+    }
 
-    // second pass
+    // second pass; resolve labels
     for (int i = 0; i < img.height; i++) {
         for (int j = 0; j < img.width; j++) {
+            // if pixel is labeled (non-zero)
             if (labelMatrix.map[i][j] != 0) {
+                // get initial label
                 int label = (int)labelMatrix.map[i][j];
-                labelMatrix.map[i][j] = labelMap[label];    // relabel collisions
+
+                // follow mapping chain to find root label
+                while (labelMap[label] != label) {
+                    label = labelMap[label];
+                }
+
+                // update label matrix with root label
+                labelMatrix.map[i][j] = label;
             }
         }
     }
@@ -331,22 +342,22 @@ Matrix labelComponents(Image img) {
     return labelMatrix;
 }
 
-//--------------------------------------------color and count components----------------------------------
+//-----------------------------------color and count components------------------------------------------
 
-// Function to generate a random color
+// generate random color
 Pixel generateRandomColor() {
     Pixel color;
     do {
-        srand(time(0));
         color.r = rand() % 256;
         color.g = rand() % 256;
         color.b = rand() % 256;
-    } while (color.r + color.g + color.b > 700 || color.r + color.g + color.b < 100);
+    } while (color.r + color.g + color.b > 600 || color.r + color.g + color.b < 200);       // ensure color not too black or white
     return color;
 }
 
-// Function to color components and count letters
-Image colorAndCountComponents(Image img, Matrix labelMatrix, int *letterCount, int sizeThreshold) {
+// color and components of image
+Image colorAndCountComponents(Image img, Matrix labelMatrix, int sizeThreshold) {
+    // get max label / number of labels
     int maxLabel = 0;
     for (int i = 0; i < labelMatrix.height; i++) {
         for (int j = 0; j < labelMatrix.width; j++) {
@@ -356,7 +367,7 @@ Image colorAndCountComponents(Image img, Matrix labelMatrix, int *letterCount, i
         }
     }
 
-    // Count the size of each component
+    // count size of each component
     int *componentSizes = (int *)calloc(maxLabel + 1, sizeof(int));
     for (int i = 0; i < labelMatrix.height; i++) {
         for (int j = 0; j < labelMatrix.width; j++) {
@@ -366,36 +377,42 @@ Image colorAndCountComponents(Image img, Matrix labelMatrix, int *letterCount, i
         }
     }
 
-    // Create a color map for components
+    // count components that meet size threshold
+    // map random color to each component
+    int count = 0;
     Pixel *colorMap = (Pixel *)malloc((maxLabel + 1) * sizeof(Pixel));
     for (int i = 1; i <= maxLabel; i++) {
-        colorMap[i] = generateRandomColor();
+        // if meets size threshold
+        if (componentSizes[i] >= sizeThreshold) {
+            colorMap[i] = generateRandomColor();
+            count++;
+        }
     }
 
-    // Create the output image
+    // create new img
     Image coloredImage = createImage(img.height, img.width);
-    int *countedLabels = (int *)calloc(maxLabel + 1, sizeof(int));
 
-    // Color the components
+    // color components
     for (int i = 0; i < labelMatrix.height; i++) {
         for (int j = 0; j < labelMatrix.width; j++) {
+            // get label
             int label = (int)labelMatrix.map[i][j];
+
+            // if labeled and component meets size threshold, then color pixel
             if (label != 0 && componentSizes[label] >= sizeThreshold) {
                 coloredImage.map[i][j] = colorMap[label];
-                // Count each label only once
-                if (countedLabels[label] == 0) {
-                    countedLabels[label] = 1;
-                    (*letterCount)++;
-                }
-            } else {
+            } else {    // otherwise, copy pixel from original img
                 coloredImage.map[i][j] = img.map[i][j];
             }
         }
     }
 
+    // deallocated memory
     free(componentSizes);
     free(colorMap);
-    free(countedLabels);
+
+    // print number of components/letters
+    printf("Number of Components: %d\n", count);
 
     return coloredImage;
 }
