b5c18e3fa664d9cb86c7a751292c39689aa56279
Michael Huh <michael.stephen.huh@gmail.com>
2024-10-30 14:27:37 -0700

    proj5

---
+++ coding/project5/project4.c
@@ -0,0 +1,26 @@
+// 
+// project5.c
+// CS136
+// 
+// project5
+// 
+// created by Michael Huh 10/30/24
+// last modified 10/30/24
+// 
+// compile:
+// gcc -o main project5.c netpbm.c
+// 
+// run:
+// ./main
+// 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "netpbm.h"
+
+int main(int argc, const char *argv[]) {
+    printf("\nProgram ends...\n\n");
+    return 0;
+}
